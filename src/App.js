e2087c86c958b10b43497f7fc1e8f78e239e8150
Mukhin, Ilya <Ilya.Mukhin@nexign-systems.com>
2019-09-02 10:59:51 +0300

    add antd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { Button } from 'antd';
+
 const App = () =>
   <div>
-    111
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+    <Button type="link">Link</Button>
   </div>
 
 export default App;
